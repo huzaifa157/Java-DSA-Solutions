25e546bb8f5b667f056ec60cc3c5f3c743d55af8
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-28 16:44:24 +0500

    array reversal by two pointer approach

--- Array/reverse.java
+++ Array/reverse.java
@@ -1,28 +1,49 @@
 public class reverse {
     public static void main(String[] args) {
 
-       int[] arr = {2,4,6,8};
-      
-       
-       reverse(arr);
+        int[] arr = { 2, 4, 6, 8 };
 
-        
+        reverse(arr);
+
+        for(int result : arr){
+     System.out.print(result + " ");
+        }
+
     }
-    
 
+    public static void reverse(int arr[]) {
 
+        int st = 0, end = arr.length - 1;
 
-public static void reverse (int arr[]){
+        while (st < end) {
 
-    int[] arr1 = new int[arr.length];
+            // swap
 
-    for(int i=0; i<arr1.length; i++){
-        arr1[i] = arr[arr.length-1-i];
+            int temp = arr[end];
+            arr[end] = arr[st];
+            arr[st] = temp;
+
+            st++;
+            end--;
+
+        }
+
     }
- for(int res : arr1){
-    System.out.print(res + " ");
- }
-    
-}
+
+
+
+
+    // public static void reverse (int arr[]){
+
+    // int[] arr1 = new int[arr.length];
+
+    // for(int i=0; i<arr1.length; i++){
+    // arr1[i] = arr[arr.length-1-i];
+    // }
+    // for(int res : arr1){
+    // System.out.print(res + " ");
+    // }
+
+    // }
 
 }
