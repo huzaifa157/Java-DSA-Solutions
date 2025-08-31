400e4610f3bd7850a3bf71478ddcb121d918c87e
huzaifa157 <m.huzaifa157@gmail.com>
2025-08-31 19:59:11 +0500

    majority element

---
+++ leetcode Questions/MajorityElement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class MajorityElement {
+    public static void main(String[] args) {
+        
+   int arr[] = {4,5,6,5,5,4,6,5,4};
+   Arrays.sort(arr);
+
+ int majority = arr[arr.length/2];
+ System.out.println(majority);
+
+
+
+
+
+
+
+    }
+    
+}
