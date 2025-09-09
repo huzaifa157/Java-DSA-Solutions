abdb88b30aa5030c1ad5e6855da98bd2d2ef46d5
huzaifa157 <m.huzaifa157@gmail.com>
2025-09-09 21:22:50 +0500

    sub array

--- Array/subarray.java
+++ Array/subarray.java
@@ -1,46 +1,49 @@
 public class subarray {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 4, 6, 8, 10 };
+        int totalSubarray = 0;
+        int t_sum = 0;
+        int maxSub = Integer.MIN_VALUE;
+        int minSub = Integer.MAX_VALUE;
     
 
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i; j < arr.length; j++) {
+
+                    int sum = 0;
+
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                    sum += arr[k];
+                  
+                }
+                totalSubarray++;
+                  t_sum+= sum;
+                System.out.println();
+                  System.out.println("sum: "+ sum );
+                  
+               if(maxSub < sum){
+                maxSub = sum;
+               }
+                if(minSub > sum){
+                    minSub = sum;
+                }
 
 
-int[] arr = {2,4,6,8,10};
 
-for(int i=0; i<arr.length; i++){
-    for(int j=i; j<arr.length; j++){
-        for(int k = i ; k<=j; k++){
-            System.out.print(arr[k]+ " ");
+
+            }
+            System.out.println();
         }
-        System.out.println();
+
+        System.out.println("Total subarrays: " + totalSubarray);
+        System.out.println("Total sum of all subarray sums: " + t_sum);
+        System.out.println("Maximum subarray sum: " + maxSub);
+        System.out.println("Minimum subarray sum: " + minSub);
+
     }
-    System.out.println();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
-
-
-
-
-
-
-
-
-
-    
-}
-
-
