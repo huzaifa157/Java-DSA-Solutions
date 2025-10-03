89a4ab76c3e27b6bf8a59753d3a44bad745b4269
huzaifa157 <m.huzaifa157@gmail.com>
2025-10-03 15:05:26 +0500

    bit manipulation

---
+++ Bit Manipulation/left_shift.java
@@ -0,0 +1,22 @@
+public class left_shift {
+    public static void main(String[] args) {
+        
+
+int a =5;
+int b = 2;
+
+System.out.println(a << b);
+
+// a<<b = a * 2^b (a multiply by 2 b square)
+          //5*2^2 = 5*4 = 20
+
+
+
+
+
+
+
+    }
+
+     
+}
---
+++ Bit Manipulation/xor.java
@@ -0,0 +1,17 @@
+public class xor {
+    public static void main(String[] args) {
+        // XOR (^) Truth Table
+// -------------------
+// 0 ^ 0 = 0   (same → 0)
+// 0 ^ 1 = 1   (different → 1)
+// 1 ^ 0 = 1   (different → 1)
+// 1 ^ 1 = 0   (same → 0)
+//
+// Shortcut uses:
+// x ^ 0 = x   (no change)
+// x ^ 1 = ~x  (toggle bit 0<->1)
+// x ^ x = 0   (self-xor = 0)
+
+    }
+    
+}
